b1efbe7473d3e5953608a3b1c5b4ea2b2550d53f
pravee2667 <praveen.sabhiniveeshu@pactera.com>
2020-01-22 16:45:52 +0530

    Updated

--- QueApiFlask/QueApiFlask/views.py
+++ QueApiFlask/QueApiFlask/views.py
@@ -6,10 +6,11 @@ from datetime import datetime
 from flask import render_template,request,jsonify
 from QueApiFlask import app
 import mysql.connector as mariadb
+import string,random
 
 
 
-connection=mariadb.connect(host='10.130.6.190',user='tescousr',password='Sp@culat1on',database='tescodb')
+connection=mariadb.connect(host='10.130.7.135',user='tescousr',password='root@123',database='tescodb')
 
 @app.route('/api/analyticsqueu')
 def APIAnalyticsQueu():
@@ -29,6 +30,74 @@ def APIAnalyticsQueu():
         inter.append(dic.copy())
     dic['inter']=inter
     return jsonify(dic)
+
+@app.route('/api/max_people')
+def max_people_for_each_queue():
+    time_r=request.args.get('interval')
+    cursor=connection.cursor()
+    cursor.execute('select queue_name,max(people_count) as people_coun from QueueCount  where time_stamp >  DATE_SUB(CURRENT_TIMESTAMP(),INTERVAL %s MINUTE) group by queue_name ',(time_r))
+    records=cursor.fetchone()
+    dic={}
+    inter=[]
+    for i in records:
+        dic={}
+        dic['queue_name']=i[0]
+        dic['count_people']=i[1]
+        inter.append(dic.copy())
+    dic['queue']=inter
+    return jsonify(dic)
+
+@app.route('/api/home')
+def select_home_queues():
+    queue=request.args.get('Queue')
+    cursor=connection.cursor()
+    if queue=="Q1":
+        cursor.execute('select * from (select Queue_Name,max(people_count) as people_coun from QueueCount  where time_stamp >  DATEADD(MINUTE ,@time,  GetDate()) group by Queue_Name ) as p where p.Queue_Name between ')
+        records=cursor.fetchone()
+        inter=[]
+        dic={}
+        for i in records:
+            dic={}
+            dic['Queue_Name']=i[0]
+            dic['people_count']=i[1]
+            inter.append(dic.copy())
+        dic['queue']=inter
+        return jsonify(dic)
+    else:
+        cursor.execute('select * from (select Queue_Name,max(people_count) as people_coun from QueueCount  where time_stamp >  DATEADD(MINUTE ,@time,  GetDate()) group by Queue_Name ) as p where p.Queue_Name between ')
+        records=cursor.fetchone()
+        inter=[]
+        dic={}
+        for i in records:
+            dic={}
+            dic['Queue_Name']=i[0]
+            dic['people_count']=i[1]
+            inter.append(dic.copy())
+        dic['queue']=inter
+        return jsonify(dic)
+
+@app.route('/api/user')
+def user_insertion():
+    letters=string.ascii_lowercase
+    user_c=''.join(random.sample(letters,4))
+    input_key=request.args.get(Key)
+    cursor=connection.cursor()
+    dic={}
+    cursor.execute('insert into QueueUser(UserName,FCM) values(%s,%s)',(user_c,input_key))
+    if(cursor.rowcount==1):
+        dic['result']='Success'
+    else:
+        dic['result']='Failed'
+    return dic
+
+
+
+
+
+
+        
+
+
 
 
 
